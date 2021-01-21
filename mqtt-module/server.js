eb5e2090c9e423905c4641e397505f8d9a0ee708
fcastiglioni <fcastiglioni@correo.um.edu.uy>
2021-01-21 11:53:54 -0300

    modulo mqtt

---
+++ mqtt-module/server.js
@@ -0,0 +1,147 @@
+'use strict'
+
+const config = require('../setup/config')
+const debug = require('debug')('platziverse:mqtt')
+const aedes = require('aedes')()
+const mongodb = require('mongodb')
+const chalk = require('chalk')
+const db = require('db-module')
+
+const { parsePayload } = require('./utils')
+
+const backend = {
+  type: 'mongodb',
+  mongodb,
+  return_buffers: true
+}
+
+const server = require('net').createServer(aedes.handle)
+const port = 1883
+
+server.listen(port, function () {
+  console.log('server started and listening on port ', port)
+})
+
+const clients = new Map()
+
+let Fridge, Metric
+
+server.on('clientConnected', client => {
+  debug(`Client Connected: ${client.id}`)
+  clients.set(client.id, null)
+})
+
+aedes.on('clientDisconnected', async (client) => {
+  debug(`Client Disconnected: ${client.id}`)
+  const fridge = clients.get(client.id)
+
+  if (fridge) {
+    // Mark Agent as Disconnected
+    fridge.connected = false
+
+    try {
+      await Fridge.createOrUpdate(fridge)
+    } catch (e) {
+      return handleError(e)
+    }
+
+    // Delete Agent from Clients list
+    clients.delete(client.id)
+
+    server.publish({
+      topic: 'fridge/disconnected',
+      payload: JSON.stringify({
+        fridge: {
+          uuid: fridge.uuid
+        }
+      })
+    })
+    debug(`Client (${client.id}) associated to Fridge (${fridge.uuid}) marked as disconnected`)
+  }
+})
+
+aedes.on('published', async (packet, client) => {
+  debug(`Received: ${packet.topic}`)
+
+  switch (packet.topic) {
+    case 'fridge/connected':
+    case 'fridge/disconnected':
+      debug(`Payload: ${packet.payload}`)
+      break
+    case 'fridge/message':
+      debug(`Payload: ${packet.payload}`)
+
+      const payload = parsePayload(packet.payload)
+
+      if (payload) {
+        payload.fridge.connected = true
+
+        let fridge
+        try {
+          fridge = await Fridge.createOrUpdate(payload.fridge)
+        } catch (e) {
+          return handleError(e)
+        }
+
+        debug(`Fridge ${fridge.uuid} saved`)
+
+        // Notify Agent is Connected
+        if (!clients.get(client.id)) {
+          clients.set(client.id, fridge)
+          server.publish({
+            topic: 'agent/connected',
+            payload: JSON.stringify({
+              fridge: {
+                uuid: fridge.uuid,
+                name: fridge.name,
+                hostname: fridge.hostname,
+                pid: fridge.pid,
+                connected: fridge.connected
+              }
+            })
+          })
+        }
+
+        // Store Metrics
+        for (const metric of payload.metrics) {
+          let m
+
+          try {
+            m = await Metric.create(fridge.uuid, metric)
+          } catch (e) {
+            return handleError(e)
+          }
+
+          debug(`Metric ${m.id} saved on agent ${fridge.uuid}`)
+        }
+      }
+      break
+  }
+
+  debug(`Payload: ${packet.payload}`)
+})
+
+aedes.on('ready', async () => {
+  const services = await db(config).catch(handleFatalError)
+
+  Fridge = services.Fridge
+  Metric = services.Metric
+
+  console.log(`${chalk.green('[mqtt]')} server is running`)
+})
+
+aedes.on('error', handleFatalError)
+
+function handleFatalError (err) {
+  console.error(`${chalk.red('[fatal error!! :( ]')} ${err.message}`)
+  console.error(err.stack)
+  process.exit(1)
+}
+
+function handleError (err) {
+  console.error(`${chalk.red('[fatal error!! :( ]')} ${err.message}`)
+  console.error(err.stack)
+}
+
+process.on('uncaughtException', handleFatalError)
+process.on('unhandledRejection', handleFatalError)
